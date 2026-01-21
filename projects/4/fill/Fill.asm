426f5e4c185169c50adc712409fe862721b0c778
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-01-21 16:19:39 +0300

    feat: add DRAW and INIT loops for Fill.asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -13,15 +13,67 @@
     @KBD
     D=M
 
-    @BLACK
-    D;JGT // if KBD > 0
-
     @color
     M=0
 
+    @SCREEN
+    D=A
+
+    @curr_addr
+    M=D
+
+    @8192
+    D=A
+
+    @counter
+    M=D
+
+    @counter
+    M=D
+
+    @KBD
+    D=M
+
+    @BLACK
+    D;JGT // if KBD > 0
+
+    @DRAW
+    0;JMP
 
 (BLACK)
     @color
     M=-1
 
-(LOOP)
+(DRAW)
+
+    @counter
+    D=M
+
+    @INIT
+    D;JEQ
+
+    @color
+    D=M
+
+    @color
+    M=D
+
+    @curr_addr
+    A=M
+    M=D
+
+    @curr_addr
+    D=M+1
+
+    @curr_addr
+    M=D
+
+    @counter
+    D=M-1
+
+    @counter
+    M=D
+
+    @DRAW
+    0;JMP
+
