ee862a77cf431f42d091c074660bc17d7fa59f74
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-01-29 10:55:54 +0300

    feat: add erasing lines and spaces for advance function

--- projects/6/assembler/src/parser.cpp
+++ projects/6/assembler/src/parser.cpp
@@ -1,11 +1,24 @@
+#include <string>
+#include <fstream>
 #include "parser.h"
 
 void Parser::advance() {
-    string line;
+    std::string line;
 
     while(std::getline(inputFile, line)) {
         
-        // 1. get co
-    }
+        // 1. Remove comments
+        size_t commontPos = line.find("//");
+        if(commentPos != std::npos) {
+            line = line.substr(0, commentPos);
+        }
+        
+        // 2. Erase all whitespaces within the line
+        std::erase_if(line, [](unsigned char c) { return std::isspace(c); });
 
+        if(line.empty()) continue;
+
+        currentCommand = line;
+        return;
+    }
 }
