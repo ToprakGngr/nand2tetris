6627b64e9cf0bff07c5c2210275a22715b857aa9
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-02-02 21:01:21 +0300

    feat: implement input and output file capture

--- projects/6/assembler/src/main.cpp
+++ projects/6/assembler/src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "parser.h"
+#include "code.h"
+#include "symboltable.h"
 
 int main(int argc, char* argv[]) {
-    return 0;
+    // if no file is given
+    if (argc != 2) {
+        std::cerr << "Usage: assembler <InputFile.asm>" << std::endl;
+        return 1;
+    }
+
+    std::string inputFileName = argv[1];
+    std::string outputFileName = inputFileName.substr(0, inputFileName.find_last_of(".")) + ".hack";     
 }
