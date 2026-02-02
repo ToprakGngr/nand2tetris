5255ee3f050e28f69f7e13ec9b9ac395a28751f1
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-02-02 19:44:34 +0300

    fix: remove several typos and logic errors

--- projects/6/assembler/src/symboltable.cpp
+++ projects/6/assembler/src/symboltable.cpp
@@ -2,8 +2,8 @@
 
 void SymbolTable::initializePreDefined() {
     for(int i = 0; i < 16; i++) {
-        std::string key = 'R' + std::to_string(i);
-        symbol_table.insert({key, 0});
+        std::string key = "R" + std::to_string(i);
+        symbol_table.insert({key, i});
     }
     symbol_table.insert({"SP", 0});
     symbol_table.insert({"LCL", 1});
@@ -12,7 +12,7 @@ void SymbolTable::initializePreDefined() {
     symbol_table.insert({"THAT", 4});
 
     symbol_table.insert({"KBD", 24576});
-    symbol_table.insert({"THAT", 16384});
+    symbol_table.insert({"SCREEN", 16384});
 }
 
 SymbolTable::SymbolTable() {
