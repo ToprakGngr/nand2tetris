33cc938b13b53f17334f46bbcba2c94d775f0791
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-02-09 21:31:59 +0300

    feat: implement advance and reset functions

--- projects/7/VMTranslator/src/parser.cpp
+++ projects/7/VMTranslator/src/parser.cpp
@@ -11,3 +11,30 @@ VMParser::VMParser(const std::string& filename) {
         exit(1);
     }
 }
+
+bool VMParser::advance() {
+    std::string line;
+
+    while(std::getline(inputFile, line)) {
+        // 1. Remove comments
+        size_t commentPos = line.find("//");
+        if(commentPos != std::string::npos) {
+            line = line.substr(0, commentPos);
+        }
+        
+        // 2. Erase all whitespaces within the line
+        std::erase_if(line, [](unsigned char c) { return std::isspace(c); });
+
+        if(line.empty()) continue;
+
+        currentCommand = line;
+        return true;
+    }
+    return false;
+}
+
+// move the cursor to the beginning
+void VMParser::reset() {
+    inputFile.clear();
+    inputFile.seekg(0);
+}
