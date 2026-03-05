1a82562506ca00686abc9cc8cf513fc3d5b9f3a8
ToprakGngr <b2210356037@cs.hacettepe.edu.tr>
2026-03-05 16:31:03 +0300

    feat: add additional instruction types to enum

--- projects/7/VMTranslator/include/types.h
+++ projects/7/VMTranslator/include/types.h
@@ -4,7 +4,13 @@
 enum InstructionType {
     PUSH_TYPE,
     POP_TYPE,
-    ARITHMETIC_TYPE
+    ARITHMETIC_TYPE,
+    LABEL_TYPE,
+    GOTO_TYPE,
+    IF_TYPE,
+    FUNCTION_TYPE,
+    RETURN_TYPE,
+    CALL_TYPE
 };
 
 #endif
